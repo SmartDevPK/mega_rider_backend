77caa61d571970d7362916d19ae45e2d6aae876a
MICHAEL EMMANUEL <emmanuelmichaelpk3@gmail.com>
2026-04-09 06:50:20 +0100

    Add configuration files for fraud detection and pricing logic

---
+++ config/fraud.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'max_package_value' => env('MAX_PACKAGE_VALUE', 500000),
+    'max_orders_per_day' => env('MAX_ORDERS_PER_DAY', 10),
+    'min_distance_km' => env('MIN_DISTANCE_KM', 0.5),
+];
---
+++ config/pricing.php
@@ -0,0 +1,26 @@
+<?php
+// config/pricing.php
+return [
+    'motorcycle' => [
+        'base_fare' => 300,
+        'rate_per_km' => 100,
+        'rate_per_min' => 5,
+    ],
+    'bike' => [
+        'base_fare' => 300,
+        'rate_per_km' => 100,
+        'rate_per_min' => 5,
+    ],
+    'car' => [
+        'base_fare' => 500,
+        'rate_per_km' => 200,
+        'rate_per_min' => 10,
+    ],
+    'van' => [
+        'base_fare' => 800,
+        'rate_per_km' => 250,
+        'rate_per_min' => 12,
+    ],
+    'insurance_percent' => 0.02,
+    'service_fee' => 100,
+];
